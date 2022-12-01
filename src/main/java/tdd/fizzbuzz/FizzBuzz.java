27f8cf968bb1637f462ef5adc0fc4812ef925612
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-01 14:57:58 +0800

    feat: return FizzBuzz when order is multiple of 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,17 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int order) {
+        String saying = "";
         if(order % 3 == 0){
-            return "Fizz";
-        } else if (order % 5 == 0) {
-            return "Buzz";
-        } else if (order % 7 == 0) {
-            return "Whizz";
+            saying += "Fizz";
         }
+        if (order % 5 == 0) {
+             saying += "Buzz";
+        }
+        if (order % 7 == 0) {
+            saying += "Whizz";
+        }
+        if(!saying.isEmpty()) return saying;
         return Integer.toString(order);
     }
 }
